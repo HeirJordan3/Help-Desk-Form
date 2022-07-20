a6cbd926b68e1d69f48e12ce9c87f1f4fe72ccf6
Jordanceldridge13 <Jordan@codedifferently.com>
2022-07-20 14:26:45 +0000

    Help Desk Form

---
+++ help-desk/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HelpDeskForm from './Components/HelpDeskForm';
+
+function App() {
+  return (
+    <>
+      <HelpDeskForm/>
+    </>
+
+    
+  );
+}
+
+export default App;
